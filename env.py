532aee96219717c0956625cb2fcc6d0cfd405253
Amourxd <126812456+Amourxd@users.noreply.github.com>
2023-03-06 15:22:02 +0530

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,8 @@ API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/link_copied")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Op_Arnav_Singh")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
